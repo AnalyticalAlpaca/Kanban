e3b20e1f8ddae6f96adef6d742d044bb69a458dc
Abass <abasssalem25@gmail.com>
2022-11-14 10:14:46 +0100

    newTask Abass

---
+++ newTask/newTask.js
@@ -0,0 +1,40 @@
+
+function select() {
+    document.getElementById('menu_links').classList.add('menu_links_selected')
+}
+
+function afterUrgent() {
+    document.getElementById('urgent').classList.add('prio-after-urgent');
+    document.getElementById('medium').classList.remove('prio-after-medium');
+    document.getElementById('low').classList.remove('prio-after-low');
+    document.getElementById('medium-img').src = "img/medium.png";
+    document.getElementById('low-img').src = "img/low.png";
+    document.getElementById('urgent-img').src = "img/urgent-after.png";
+}
+
+function afterMedium() {
+    document.getElementById('medium').classList.add('prio-after-medium');
+    document.getElementById('low').classList.remove('prio-after-low');
+    document.getElementById('urgent').classList.remove('prio-after-urgent');
+    document.getElementById('low-img').src = "img/low.png";
+    document.getElementById('urgent-img').src = "img/urgent.png";
+    document.getElementById('medium-img').src = "img/medium-after.png";
+}
+
+function afterLow() {
+    document.getElementById('low').classList.add('prio-after-low');
+    document.getElementById('medium').classList.remove('prio-after-medium');
+    document.getElementById('urgent').classList.remove('prio-after-urgent');
+    document.getElementById('urgent-img').src = "img/urgent.png";
+    document.getElementById('medium-img').src = "img/medium.png";
+    document.getElementById('low-img').src = "img/low-after.png";
+}
+
+function changeBtn(){
+    document.getElementById('subtask').classList.remove('input-trans')
+    document.getElementById('subtask').classList.add('input-trans-after')
+    document.getElementById('addBtn1').classList.add('d-none')
+    document.getElementById('addBtn2').classList.remove('d-none')
+    document.getElementById('addBtn3').classList.remove('d-none')
+    document.getElementById('vl2').classList.remove('d-none')  
+}
