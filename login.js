4eac73eadd28fc65dfe6aa6ad9c79f11157e5bfd
BettinaNestler <bettina.nestler84@gmail.com>
2022-11-15 15:32:32 +0100

    Login Design

---
+++ login.js
@@ -0,0 +1,9 @@
+function loadSignUp(){
+    document.getElementById('login_container').classList.add('d-none');
+    document.getElementById('signup_container').classList.remove('d-none');
+}
+
+function loadSignIn(){
+    document.getElementById('login_container').classList.remove('d-none');
+    document.getElementById('signup_container').classList.add('d-none');
+}
