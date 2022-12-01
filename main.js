f4c4c0c14068bc5ecd0a79ca90f3bfbe87e09c3a
Dimitri Gontscharenko <dima.webdevelopment@gmail.com>
2022-12-01 14:30:00 +0100

    InfoBox

--- main.js
+++ main.js
@@ -283,8 +283,8 @@ function closeRenderCategoryInfo() {
 }
 
 function changeCategoryInfo(task) {
-    let titleUpdate = document.getElementById('titleValue').value;
-    let descriptionUpdate = document.getElementById('descriptionValue').value;
+    // let titleUpdate = document.getElementById('titleValue').value;
+    // let descriptionUpdate = document.getElementById('descriptionValue').value;
     document.getElementById('fullscreen').innerHTML = /*html*/ `
     <div class="infoWindow changeInfo" style="padding: 3rem 6rem 6rem 6rem">
         <img class="close_icon" src="img/closeImage.png" alt="close-image" onclick="closeRenderCategoryInfo()">
@@ -296,13 +296,13 @@ function changeCategoryInfo(task) {
         <input type="date" class="info_section">
         <p>Prio</p>
         <p>Assigned to</p>
-        <img class="change_icon" src="img/changeImage2.png" alt="change-image" onclick="renderCategoryInfo(taskForCategoryInfo)">
+        <img class="change_icon change_icon2" src="img/changeImage2.png" alt="change-image" onclick="renderCategoryInfo(taskForCategoryInfo)">
     </div>
     `;
 
-    if (titleUpdate != tasks.title) {
-        titleUpdate.push
-    }
+    // if (titleUpdate != tasks.title) {
+    //     titleUpdate.push
+    // }
 
     document.getElementById('titleValue').value = `${task.title}`;
     document.getElementById('descriptionValue').value = `${task.description}`;
