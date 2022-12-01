e3060f8301010421815fc75f386336c1d69f56a6
BettinaNestler <bettina.nestler84@gmail.com>
2022-12-01 08:53:49 +0100

    update logout

--- nav.js
+++ nav.js
@@ -4,6 +4,12 @@ function renderLogoutBox(){
     document.getElementById('display_column').classList.add('display_column_clicked');
 }
 
+function renderLogoutBoxMobil(){
+    document.getElementById('logout_box_mobil').classList.remove('d-none');
+    document.getElementById('display_column_mobil').classList.remove('display_column_mobil');
+    document.getElementById('display_column_mobil').classList.add('display_column_clicked');
+}
+
 async function logout(){    
    
     window.location.href =`index.html?msg=You have logged out.`;
